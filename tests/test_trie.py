995d8096b368d4291740f656e06a40d6585a7517
lojoja <dev@lojoja.com>
2024-02-07 21:39:17 -0500

    Add test case for mixed valid/invalid values with silenced errors

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -7,31 +7,37 @@ from triex.triex import _TrieNode, _DataInput
 
 
 @pytest.mark.parametrize(
-    ["init_data", "extra_data", "valid", "members", "structure"],
+    ["init_data", "extra_data", "valid", "members", "structure", "silent"],
     [
-        (None, [], True, [], {}),
-        ([], [], True, [], {}),
-        ("foo", [], True, ["foo"], {"f": {"o": {"o": {"": {}}}}}),
+        (None, [], True, [], {}, True),
+        ([], [], True, [], {}, True),
+        ("foo", [], True, ["foo"], {"f": {"o": {"o": {"": {}}}}}, True),
         (
             ["foo", 1, 1.0],
             [],
             True,
             ["1", "1.0", "foo"],
             {"f": {"o": {"o": {"": {}}}}, "1": {".": {"0": {"": {}}}, "": {}}},
+            True,
         ),
-        ("foo", ["foo", "bar"], True, ["bar", "foo"], {"f": {"o": {"o": {"": {}}}}, "b": {"a": {"r": {"": {}}}}}),
-        ([], [None], False, [], {}),
+        ("foo", ["foo", "bar"], True, ["bar", "foo"], {"f": {"o": {"o": {"": {}}}}, "b": {"a": {"r": {"": {}}}}}, True),
+        ([], [None], False, [], {}, False),
+        ([], ["foo", None], True, ["foo"], {"f": {"o": {"o": {"": {}}}}}, True),
     ],
-    ids=["no value (None)", "no value (list)", "single value", "multiple values", "duplicate values", "invalid values"],
+    ids=[
+        "no value (None)",
+        "no value (list)",
+        "single value",
+        "multiple values",
+        "duplicate values",
+        "invalid values",
+        "valid+invalid values (silent)",
+    ],
 )
 def test_trie_data(
-    init_data: _DataInput,
-    extra_data: _DataInput,
-    valid: bool,
-    members: list[str],
-    structure: _TrieNode,
+    init_data: _DataInput, extra_data: _DataInput, valid: bool, members: list[str], structure: _TrieNode, silent: bool
 ):
-    trie = Trie(init_data, silent=valid)
+    trie = Trie(init_data, silent=silent)
 
     if extra_data:
         if valid:
